67f6de86bd56174a6ae645dfaa87125f3cd9a491
Diego <alejorvela@gmail.com>
2021-09-03 14:41:15 -0500

    update functions

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -115,9 +115,54 @@ const listUserAll = async (req, res) => {
         return res.status(400).send("No search results");
     return res.status(200).send({ users });
 };
-const getRole = async (req, res) => { };
-const updateUser = async (req, res) => { };
-const deleteUser = async (req, res) => { }; //se actualiza el estado a false
+const getRole = async (req, res) => {
+  const user = await User.findOne({ email: req.params.email })
+    .populate("roleId")
+    .exec();
+  if (!user || user.length === 0)
+    return res.status(400).send("Error: User no found");
+  const role = user.roleId.name;
+  return res.status(200).send({ role });
+};
+const updateUser = async (req, res) => {
+  if (!req.body._id || !req.body.name || !req.body.email || !req.body.roleId)
+    return res.status(400).send("Error: Empty fields");
+
+  let pass = "";
+  let imageUrl = "";
+
+  if (req.body.password) {
+    pass = await bcrypt.hash(req.body.password, 10);
+  } else {
+    const userFind = await User.findOne({ email: req.body.email });
+    pass = userFind.password;
+  }
+
+  if (req.files.image) {
+    if (req.files.image.type != null) {
+      const url = req.protocol + "://" + req.get("host") + "/";
+      const serverImg =
+        "./uploads/" + moment().unix() + path.extname(req.files.image.path);
+      fs.createReadStream(req.files.image.path).pipe(
+        fs.createWriteStream(serverImg)
+      );
+      imageUrl =
+        url + "uploads/" + moment().unix() + path.extname(req.files.image.path);
+    }
+  }
+
+  const user = await User.findByIdAndUpdate(req.body._id, {
+    name: req.body.name,
+    email: req.body.email,
+    password: pass,
+    roleId: req.body.roleId,
+    userImg: imageUrl,
+  });
+
+  if (!user) return res.status(400).send("Error editing user");
+  return res.status(200).send({ user });
+};
+const deleteUser = async (req, res) => {}; //se actualiza el estado a false
 
 module.exports = {
     createAdmin,
