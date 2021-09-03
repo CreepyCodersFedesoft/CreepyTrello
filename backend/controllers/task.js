ce2d7293937f6759a22083f4be7da29bf90ac437
Jairo Alzate <jairo.alzate@siesa.com>
2021-09-03 14:31:45 -0500

    Add update and delete task

--- backend/controllers/task.js
+++ backend/controllers/task.js
@@ -2,6 +2,7 @@ const Task = require("../models/task");
 const fs = require("fs");
 const path = require("path");
 const moment = require("moment");
+const mongoose = require("mongoose");
 
 const createTask = async (req, res) => {
   if (!req.body.boardId || !req.body.title || !req.body.description)
@@ -35,13 +36,50 @@ const createTask = async (req, res) => {
 };
 
 const listTask = async (req, res) => {
-    const task = await Task.find({ boardId: req.params.boardId });
-    if(!task || task.length == 0) return res.status(400).send("You have no assigned task");
-    return res.status(200).send({ task });
+  const task = await Task.find({ boardId: req.params.boardId });
+  if (!task || task.length == 0)
+    return res.status(400).send("You have no assigned task");
+  return res.status(200).send({ task });
 };
 
-const updateTask = async (req, res) => {};
+const updateTask = async (req, res) => {
+  let validId = mongoose.Types.ObjectId.isValid(req.body._id);
+  if (!validId) return res.status(400).send("Invalid id");
 
-const deleteTask = async (req, res) => {}; //este si elimina definitivamente
+  if (!req.body.title || !req.body.description || !req.body.taskStatus || !req.body.userId)
+    return res.status(400).send("Process failed: Incomplete data");
 
-module.exports = { createTask, listTask, updateTask, deleteTask,};
+  const task = await Task.findByIdAndUpdate(req.body._id, {
+    userId: req.body.userId,
+    title: req.body.title,
+    description: req.body.description,
+    taskStatus: req.body.taskStatus,
+  });
+
+  if (!task) return res.status(400).send("Task not found");
+  return res.status(200).send({ task });
+};
+
+const deleteTask = async (req, res) => {
+  const validId = mongoose.Types.ObjectId.isValid(req.params._id);
+  if (!validId) return res.status(400).send("Invalid id");
+ 
+
+  const task = await Task.findByIdAndDelete(req.params._id);
+  if (!task) return res.status(400).send("Task not found");
+  /*
+  let taskImg = await Task.findById(req.params._id);
+  taskImg = taskImg.imageUrl;
+  taskImg = taskImg.split("/")[4];
+  let serverImg = "./uploads/" + taskImg;
+
+  try {
+    fs.unlinkSync(serverImg);
+  } catch (e) {
+    console.log("Image no found in server");
+  }
+  */
+  return res.status(200).send({ message: "Task deleted" });
+}; 
+
+module.exports = { createTask, listTask, updateTask, deleteTask };
