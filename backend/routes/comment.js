2691ce575e211ef4339a059acf8770deec10f64b
Diego <alejorvela@gmail.com>
2021-09-07 13:42:58 -0500

    fixed routes comment

--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -3,12 +3,8 @@ const router = require('express').Router();
 const auth = require('../middleware/auth');
 const validateUser = require('../middleware/validateUser');
 
-<<<<<<< HEAD
 router.post('/createComment', auth, validateUser, CommentController.createComment);
-=======
-router.post('/createComment', CommentController.createComment);
-router.post('/updateComment', CommentController.updateComment);
->>>>>>> 65815399b8317d4478eba353420cc3ea34563db1
+router.put('/updateComment', CommentController.updateComment);
 router.get('/listComment', CommentController.listComment);
 router.delete('/deleteComment/:_id', CommentController.deleteComment);
 
