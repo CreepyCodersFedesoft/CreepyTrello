0d4487f0ea8f5b46742866a0d213023c3b850819
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2021-09-07 08:27:05 -0500

    cambios del controller comment

--- backend/controllers/comment.js
+++ backend/controllers/comment.js
@@ -1,7 +1,23 @@
-const Comment = require('../models/comment');
+const Comment = require("../models/comment");
 
-const createComment = async (req, res) => {}
-const listComment = async (req, res) => {}
-const deleteComment = async (req, res) => {} //se elimina definitivamente
+
+const createComment = async (req, res) => {
+  if (!req.body.text) return res.status(400).send("Incomplete Data");
+
+  const comment = new Comment({
+    userId: req.user._id,
+    boardId: req.board._id,
+    text: req.body.text,
+    dbStatus: true,
+  });
+
+  const result = await comment.save();
+  if(!result) return res.status(400).send("Error registering comment");
+  return res.status(200).send({result});
+}; 
+
+
+const listComment = async (req, res) => {};
+const deleteComment = async (req, res) => {}; //se elimina definitivamente
 
 module.exports = { createComment, listComment, deleteComment };
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -1,7 +1,9 @@
 const CommentController = require('../controllers/comment');
 const router = require('express').Router();
+const auth = require('../middleware/auth');
+const validateUser = require('../middleware/validateUser');
 
-router.post('/createComment', CommentController.createComment);
+router.post('/createComment', auth, validateUser, CommentController.createComment);
 router.get('/listComment', CommentController.listComment);
 router.delete('/deleteComment/:_id', CommentController.deleteComment);
 
