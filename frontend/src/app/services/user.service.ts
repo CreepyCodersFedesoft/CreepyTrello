5292d59841b12bd51f88b74f5ac9a87512864cdd
Jairo Alzate <jairo.alzate@siesa.com>
2021-09-08 10:36:29 -0500

    Verify login to show button in header

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private _router: Router) { }
+
+
+  loggedIn() {
+    return !!localStorage.getItem('token');
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this._router.navigate(['/login']);
+  }
 }
