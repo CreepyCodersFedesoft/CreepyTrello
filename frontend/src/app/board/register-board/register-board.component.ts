f6967adaf8122c87991a00d5bd899ad52ab2cc6f
jcgomez-Helppeople <jgomezberrio054@gmail.com>
2021-09-10 11:17:37 -0500

    creating board is added

--- frontend/src/app/board/register-board/register-board.component.ts
+++ frontend/src/app/board/register-board/register-board.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { BoardService } from "../../services/board.service";
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register-board',
@@ -15,4 +21,13 @@ export class RegisterBoardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  saveTask(){
+    if(!this.registerBoard.name || !this.registerBoard.description){
+
+    }
+  }
+
+  openSnackBarError(){
+    
+  }
 }
