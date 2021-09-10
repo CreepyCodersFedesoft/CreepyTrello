cb15d103aa4a72cd5ca32353732db0b7995fc7da
Diana Eleira Triana Suarez <dianaeleira@gmail.com>
2021-09-10 10:03:39 -0500

    backup sendgrid email tests

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const User = require('./routes/user');
 const Board = require('./routes/board');
 const Task = require('./routes/task');
 const Comment = require('./routes/comment');
+const SendMail = require('./routes/sendgrid');
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use("/api/user", User);
 app.use("/api/board", Board);
 app.use("/api/task", Task);
 app.use("/api/comment", Comment);
+app.use("/api/sendMail", SendMail);
 
 app.listen(
   process.env.PORT || 3000, () =>
