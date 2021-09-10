29d7241eae1d6bd5f34c3d94661d91ee2b2b6221
Jairo Alzate <jairo.alzate@siesa.com>
2021-09-10 15:53:04 -0500

    Change visual things

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,15 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
-
-
 import { UserService } from './services/user.service';
 import { RoleService } from './services/role.service';
 import { BoardService } from './services/board.service';
-import { TaskService } from "./services/task.service";
-import { CommentService } from "./services/comment.service";
-import { UtilitiesService } from "./services/utilities.service";
+import { TaskService } from './services/task.service';
+import { CommentService } from './services/comment.service';
+import { UtilitiesService } from './services/utilities.service';
 import { TokenInterceptorService } from './services/token-interceptor.service';
 import { AuthGuard } from './guard/auth.guard';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -33,7 +30,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {DragDropModule} from '@angular/cdk/drag-drop';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { HeaderComponent } from './home/header/header.component';
 import { RegisterComponent } from './home/register/register.component';
@@ -51,8 +48,6 @@ import { ListRoleComponent } from './admin/list-role/list-role.component';
 import { ListUserComponent } from './admin/list-user/list-user.component';
 import { ListSpringComponent } from './board/spring/list-spring/list-spring.component';
 import { CreateSpringComponent } from './board/spring/create-spring/create-spring.component';
-
-
 
 @NgModule({
   declarations: [
