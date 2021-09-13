0c6b7577fd8e5ef2e444b2c14c7980acf18118ce
jcgomez-Helppeople <jgomezberrio054@gmail.com>
2021-09-13 10:34:14 -0500

    tests with create board

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -7,9 +7,9 @@ const auth = require('../middleware/auth');
 const validateUser = require('../middleware/validateUser');
 const invitedUser = require('../middleware/invitedUser');
 
-router.post('/createBoard', auth, validateUser, mult, Upload, BoardController.createBoard);
+router.post('/createBoard', auth, validateUser, mult, Upload,  BoardController.createBoard);
 router.get('/listBoard',  auth, validateUser, BoardController.listBoard);
-router.put('/updateBoard', auth, validateUser, invitedUser, mult, Upload, BoardController.updateBoard);
+router.put('/updateBoard', auth, validateUser, invitedUser, mult, Upload,   BoardController.updateBoard);
 router.put('/addListBoard', auth, validateUser, BoardController.addListBoard);
 router.put('/dropListBoard', auth, validateUser, BoardController.dropListBoard);
 router.delete('/deleteBoard/:_id', auth, validateUser, BoardController.deleteBoard);
--- frontend/src/app/board/register-board/register-board.component.ts
+++ frontend/src/app/board/register-board/register-board.component.ts
@@ -14,20 +14,53 @@ import {
 })
 export class RegisterBoardComponent implements OnInit {
   registerBoard: any;
-  constructor() {
+  message: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number = 2;
+  constructor(
+    private _boardService: BoardService,
+    private _router: Router,
+    private _snackBar: MatSnackBar
+  ) {
     this.registerBoard = {};
   }
 
   ngOnInit(): void {
   }
 
-  saveTask(){
+  createBoard(){
     if(!this.registerBoard.name || !this.registerBoard.description){
-
+      this.message = 'Fallo el proceso: Datos icompletos';
+      this.openSnackBarError();
+      this.registerBoard = {};
+    }else{
+      this._boardService.createBoard(this.registerBoard).subscribe(
+        (res)=>{
+          this._router.navigate(['/listBoard']);
+          this.message = 'Tablero Creado';
+          this.openSnackBarSuccesfull();
+          this.registerBoard = {};
+        }
+      )
     }
   }
 
+  openSnackBarSuccesfull(){
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition:this.horizontalPosition,
+      verticalPosition:this.verticalPosition,
+      duration: this.durationInSeconds *1000,
+      panelClass: ['style-snackBarTrue'],
+    });
+  }
+
   openSnackBarError(){
-    
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarFalse']
+    });
   }
 }
