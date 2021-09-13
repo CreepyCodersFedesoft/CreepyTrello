ff86b84a4bbee30dc826175558aeeb46958676e9
Diego <alejorvela@gmail.com>
2021-09-13 14:57:02 -0500

    added update and delete sprint on backend, deletesprint on frontend and sweetalert2

--- frontend/src/app/board/spring/list-spring/list-spring.component.ts
+++ frontend/src/app/board/spring/list-spring/list-spring.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { UtilitiesService } from 'src/app/services/utilities.service';
-import { SpringService } from "../../../services/spring.service";
+import { SpringService } from '../../../services/spring.service';
 import { CreateTaskComponent } from '../../task/create-task/create-task.component';
 import { CreateSpringComponent } from '../create-spring/create-spring.component';
+import swal from 'sweetalert2';
+
 @Component({
   selector: 'app-list-spring',
   templateUrl: './list-spring.component.html',
-  styleUrls: ['./list-spring.component.css']
+  styleUrls: ['./list-spring.component.css'],
 })
 export class ListSpringComponent implements OnInit {
   show: boolean = true;
@@ -22,52 +24,50 @@ export class ListSpringComponent implements OnInit {
     private _router: Router,
     private _activeRoute: ActivatedRoute,
     private _matDialog: MatDialog,
-    private _utilitiesService: UtilitiesService,
-  ) { 
+    private _utilitiesService: UtilitiesService
+  ) {
     this.springData = {};
     this.boardData = {};
     this.springId = null;
-    this.message = "";
+    this.message = '';
   }
 
   ngOnInit(): void {
-    
     //debe llegar el id del board y en base a este listar los springs
-    this._springService.listSpring(this._activeRoute.snapshot.params.boardId).subscribe(
-      (res) => {
-        console.log(res);
-        if(res.spring.length === 0){
-          this.message = 'No hay Springs creados en este board'
-          this._utilitiesService.openSnackBarError(this.message);
-        } else {
-          this.springData = res;
-          this.boardData._id = this._activeRoute.snapshot.params.boardId;
-          this.boardData.boardImg = res.spring[0].boardId.boardImg;
-          this.boardData.name = res.spring[0].boardId.name;
-          this.boardData.description = res.spring[0].boardId.description;
-          console.log(this.boardData);         
-        }
-      },
-      (err) => {
-        
-      }
-    );
+    this._springService
+      .listSpring(this._activeRoute.snapshot.params.boardId)
+      .subscribe(
+        (res) => {
+          console.log(res);
+          if (res.spring.length === 0) {
+            this.message = 'No hay Springs creados en este board';
+            this._utilitiesService.openSnackBarError(this.message);
+          } else {
+            this.springData = res;
+            this.boardData._id = this._activeRoute.snapshot.params.boardId;
+            this.boardData.boardImg = res.spring[0].boardId.boardImg;
+            this.boardData.name = res.spring[0].boardId.name;
+            this.boardData.description = res.spring[0].boardId.description;
+            console.log(this.boardData);
+          }
+        },
+        (err) => {}
+      );
   }
 
   ngOnChanges(entryId: string) {
     this.chargeSpring(entryId);
   }
 
-  chargeSpring(springId: any ){
+  chargeSpring(springId: any) {
     this.springId = springId;
   }
 
-  
-  onCreate(){
-    const matDialog= new MatDialogConfig();
-    matDialog.disableClose=true;
-    matDialog.autoFocus=true;
-    matDialog.width="50%";
+  onCreate() {
+    const matDialog = new MatDialogConfig();
+    matDialog.disableClose = true;
+    matDialog.autoFocus = true;
+    matDialog.width = '50%';
     this._matDialog.open(CreateSpringComponent, matDialog);
   }
 
@@ -78,12 +78,30 @@ export class ListSpringComponent implements OnInit {
   addTask(springId: string, boardId: string) {
     const matDialog = new MatDialogConfig();
     this._matDialog.open(CreateTaskComponent, {
-      data: {springId, boardId},
+      data: { springId, boardId },
       autoFocus: true,
       panelClass: [''],
       width: '400px',
       height: '400px',
     });
     this.chargeSpring(springId);
+  }
+
+  async deleteSprint() {
+    let result = await swal.fire({
+      title: '¿Esta seguro de que desea eliminar el sprint seleccionado?',
+      text: "¡No serás capaz de revertir estos cambios!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '¡Si, Eliminalo!',
+    });
+
+    if (result.isConfirmed) {
+      this._springService.deleteSpring(this.springId).subscribe();
+      swal.fire('Proceso Exitoso', '¡Sprint eliminado con existo!', 'success')
+      this.ngOnInit();
+    }
   }
 }
