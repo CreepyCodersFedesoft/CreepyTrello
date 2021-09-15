0bb23ae994317672e528b3f48d770fdc89530f45
Diego <alejorvela@gmail.com>
2021-09-15 12:30:31 -0500

    fix dates validation on springs

--- backend/controllers/board.js
+++ backend/controllers/board.js
@@ -133,7 +133,7 @@ const deleteBoard = async (req, res) => {
   }
   return res.status(200).send({ message: "Board deleted" });
 };
-const addListBoard = async (req, res) => {
+const addListBoard = async (req, res) => {//añade un usuario invitado al board
   if (!req.body._id || !req.body.newUserId)
     return res.status(400).send("Error: empty data");
 
@@ -159,7 +159,7 @@ const addListBoard = async (req, res) => {
 
   res.status(200).send({ board });
 };
-const dropListBoard = async (req, res) => {
+const dropListBoard = async (req, res) => {//borrar a un usuario invitado del board
   if (!req.body._id || !req.body.newUserId)
     return res.status(400).send("Error: empty data");
 
--- backend/routes/comment.js
+++ backend/routes/comment.js
@@ -4,8 +4,8 @@ const auth = require('../middleware/auth');
 const validateUser = require('../middleware/validateUser');
 
 router.post('/createComment', auth, validateUser, CommentController.createComment);
-router.put('/updateComment', CommentController.updateComment);
-router.get('/listComment', CommentController.listComment);
-router.delete('/deleteComment/:_id', CommentController.deleteComment);
+router.put('/updateComment', auth, validateUser, CommentController.updateComment);
+router.get('/listComment', auth, validateUser, CommentController.listComment);
+router.delete('/deleteComment/:_id', auth, validateUser, CommentController.deleteComment);
 
 module.exports = router;
