41a19d94457fb46c5e9b772349625d62d3d695b1
Jairo Alzate <jairo.alzate@siesa.com>
2021-09-17 09:17:28 -0500

    list users

--- frontend/src/app/admin/list-user/list-user.component.ts
+++ frontend/src/app/admin/list-user/list-user.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit, ViewChild  } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { UtilitiesService } from 'src/app/services/utilities.service';
-import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-list-user',
@@ -11,7 +11,7 @@ import { MatTableDataSource } from '@angular/material/table';
   styleUrls: ['./list-user.component.css']
 })
 export class ListUserComponent implements OnInit {
-  displayedColumns: string[] = ['Nombre', 'Email'];
+  displayedColumns: string[] = ['Foto', 'Nombre', 'Email', 'Role'];
   dataSource: MatTableDataSource<any>;
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
@@ -28,6 +28,7 @@ export class ListUserComponent implements OnInit {
     this._userService.getAllUser().subscribe(
       (res) => {
         this.userData = res.users;
+        console.log(res.users);
         this.dataSource = new MatTableDataSource(this.userData);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
